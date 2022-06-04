e5958f68c201e56e6e794abbe07e0f6f238d2f99
DawnTrick <ahmetbilalturan1@gmail.com>
2022-06-04 22:14:40 +0300

    uploaded_cbz_file
    try open and show images inside it later

---
+++ lib/widget/inspect_webtoon.dart
@@ -0,0 +1,14 @@
+import 'package:archive/archive.dart';
+import 'package:flutter/material.dart';
+
+class InspectWebtoon extends StatefulWidget {
+  const InspectWebtoon({Key? key}) : super(key: key);
+
+  @override
+  State<InspectWebtoon> createState() => _InspectWebtoonState();
+}
+
+class _InspectWebtoonState extends State<InspectWebtoon> {
+  @override
+  Widget build(BuildContext context) {}
+}
